cc704f3d9b6e29bb01a32951c108dcf30b60ef40
rxmxsxg <rameshgongura@gmail.com>
2025-08-14 20:22:27 +0530

    spring security initial project

---
+++ src/test/java/com/tharunika/tech/springsecurity/SpringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tharunika.tech.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
